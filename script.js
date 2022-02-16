723ef87fffc295917da1eb6ded74008a61cdfc8a
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2022-02-16 22:06:33 +0900

    add header

--- script.js
+++ script.js
@@ -3,17 +3,11 @@
     const elements = document.getElementsByClassName('headerSearch');
     const header = document.getElementById('header');
     for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener('mousemove', (e) => {
-            // header.style.display = "block";
-            if (e) {
-                // alert('hi');
-                header.style.display = "block";
-            } else if (!e) {
-                console.log('hi')
-            }
-        })
-        // elements[i].addEventListener('mousemove', () => {
-        //     document.getElementById('header').style.display = "block";
-        // });
+        elements[i].addEventListener('mouseover', () => {
+            header.style.top = '80px';
+        });
+        elements[i].addEventListener('mouseleave', () => {
+            header.style.top = '0';
+        });
     }
 }
